b0cda7e89660dbf15d6e4678232824ee89af0846
Fortyseven <fortyseven@gmail.com>
2015-07-23 13:48:18 -0400

    Minor formatting changes

--- MainApp/src/main/java/com/bytestemplar/tonedef/TonePanelActivity.java
+++ MainApp/src/main/java/com/bytestemplar/tonedef/TonePanelActivity.java
@@ -42,45 +42,45 @@ public class TonePanelActivity extends Activity
     private HashMap<Integer, ToneSequence> _about_buttons;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
+    protected void onCreate( Bundle savedInstanceState )
     {
-        super.onCreate(savedInstanceState);
+        super.onCreate( savedInstanceState );
 
-        this.setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        this.setVolumeControlStream( AudioManager.STREAM_MUSIC );
 
         this._momentary_buttons = new HashMap<Integer, ToneSequence>();
         this._about_buttons = new HashMap<Integer, ToneSequence>();
 
-        this._ttf_dxb = Typeface.createFromAsset(this.getAssets(), "c64.ttf");
+        this._ttf_dxb = Typeface.createFromAsset( this.getAssets(), "c64.ttf" );
 
         this._button_press = new OnTouchListener()
         {
             @Override
-            public boolean onTouch(View v, MotionEvent event)
+            public boolean onTouch( View v, MotionEvent event )
             {
                 int id = v.getId();
 
-                if (TonePanelActivity.this._momentary_buttons.containsKey(id)) {
-                    switch (event.getAction()) {
+                if ( TonePanelActivity.this._momentary_buttons.containsKey( id ) ) {
+                    switch ( event.getAction() ) {
                         case MotionEvent.ACTION_DOWN:
-                            TonePanelActivity.this._momentary_buttons.get(id).start();
+                            TonePanelActivity.this._momentary_buttons.get( id ).start();
                             break;
                         case MotionEvent.ACTION_CANCEL:
                         case MotionEvent.ACTION_UP:
-                            TonePanelActivity.this._momentary_buttons.get(id).stop();
+                            TonePanelActivity.this._momentary_buttons.get( id ).stop();
                             break;
                     }
                 }
-                if (TonePanelActivity.this._about_buttons.containsKey(id)) {
-                    switch (event.getAction()) {
+                if ( TonePanelActivity.this._about_buttons.containsKey( id ) ) {
+                    switch ( event.getAction() ) {
                         case MotionEvent.ACTION_DOWN:
-                            if (!TonePanelActivity.this._about_buttons.get(id)
-                                                                      .getDescription()
-                                                                      .equals("")) {
-                                Toast.makeText(v.getContext(),
-                                               TonePanelActivity.this._about_buttons.get(id)
-                                                                                    .getDescription(),
-                                               Toast.LENGTH_LONG).show();
+                            if ( !TonePanelActivity.this._about_buttons.get( id )
+                                                                       .getDescription()
+                                                                       .equals( "" ) ) {
+                                Toast.makeText( v.getContext(),
+                                                TonePanelActivity.this._about_buttons.get( id )
+                                                                                     .getDescription(),
+                                                Toast.LENGTH_LONG ).show();
                             }
                             break;
                     }
@@ -92,65 +92,65 @@ public class TonePanelActivity extends Activity
 
     }
 
-    protected void modifyViews(ViewGroup l)
+    protected void modifyViews( ViewGroup l )
     {
-        for (int i = 0; i < l.getChildCount(); i++) {
-            View v = l.getChildAt(i);
-            if (v instanceof Button) {
-                this.updateBitmap((Button) v);
+        for ( int i = 0; i < l.getChildCount(); i++ ) {
+            View v = l.getChildAt( i );
+            if ( v instanceof Button ) {
+                this.updateBitmap( (Button) v );
             }
-            if (v instanceof LinearLayout) {
-                this.modifyViews((ViewGroup) v);
+            if ( v instanceof LinearLayout ) {
+                this.modifyViews( (ViewGroup) v );
             }
-            if (v instanceof ScrollView) {
-                this.modifyViews((ViewGroup) v);
+            if ( v instanceof ScrollView ) {
+                this.modifyViews( (ViewGroup) v );
             }
-            if (v instanceof TextView) {
-                this.updateText((TextView) v);
+            if ( v instanceof TextView ) {
+                this.updateText( (TextView) v );
             }
         }
     }
 
-    protected void updateBitmap(Button btn)
+    protected void updateBitmap( Button btn )
     {
-        btn.setOnTouchListener(this._button_press);
+        btn.setOnTouchListener( this._button_press );
 //        btn.setTypeface( this._ttf_dxb );
 //        btn.setSoundEffectsEnabled( false );
     }
 
-    protected void updateText(TextView btn)
+    protected void updateText( TextView btn )
     {
-        btn.setTypeface(this._ttf_dxb);
+        btn.setTypeface( this._ttf_dxb );
     }
 
-    protected void setTitleText(String title)
+    protected void setTitleText( String title )
     {
         // TextView tv = (TextView) findViewById( R.id._title );
         // tv.setText( _title );
 
-        this.setTitle(this.getTitle() + " - " + title);
+        this.setTitle( this.getTitle() + " - " + title );
 
     }
 
     protected void setup()
     {
-        LinearLayout root = (LinearLayout) this.findViewById(R.id.root);
-        if (root == null) {
+        LinearLayout root = (LinearLayout) this.findViewById( R.id.root );
+        if ( root == null ) {
             try {
-                throw new Exception("Require a LinearLayout with id of 'root'");
+                throw new Exception( "Require a LinearLayout with id of 'root'" );
             }
-            catch (Exception e) {
+            catch ( Exception e ) {
                 e.printStackTrace();
             }
         }
-        this.modifyViews(root);
+        this.modifyViews( root );
 
     }
 
-    protected void defineButton(int id, int about_id, ToneSequence tonegen)
+    protected void defineButton( int id, int about_id, ToneSequence tonegen )
     {
-        this._momentary_buttons.put(id, tonegen);
-        this._about_buttons.put(about_id, tonegen);
+        this._momentary_buttons.put( id, tonegen );
+        this._about_buttons.put( about_id, tonegen );
     }
 
 }
