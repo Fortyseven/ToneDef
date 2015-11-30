48a5e0bfcf569d57e07bf8b9b2256d0c8f942567
Fortyseven <fortyseven@gmail.com>
2015-11-30 17:38:43 -0500

    Consoldated description strings; swapped dialtone/ringback for Italy; cleaned cruft

--- MainApp/src/main/java/com/bytestemplar/tonedef/tones/OtherTones.java
+++ MainApp/src/main/java/com/bytestemplar/tonedef/tones/OtherTones.java
@@ -30,40 +30,45 @@ public class OtherTones
     public ToneSequence _de_dialtone;
 
     public String _title = "Other Telephony";
-//    private final Activity _parent;
+
+    private final int ITALY_FREQ = 425;
+
+    // TODO: Move these into a string resource. However, this whole section is going to be redone at some point, so it might not even be worth it.
+
+    private final String DIALTONE_DESC = "A dial tone is a telephony signal used to indicate that the telephone exchange is " +
+                                         "working, has recognized an off-hook, and is ready to accept a call.";
+
+    private final String RINGBACK_DESC = "A ringback tone is an audible indication that is heard on the telephone line by " +
+                                         "the caller while the phone they are calling is being rung. It is normally a repeated " +
+                                         "tone, designed to assure the calling party that the called party's line is ringing, " +
+                                         "although the ring-back tone may be out of sync with the ringing signal.";
 
     public OtherTones( Activity parent )
     {
-//        this._parent = parent;
         _de_dialtone = new ToneSequence( parent );
         _de_dialtone.addSegment( 250, 425 );
-        _de_dialtone.setDescription(
-                "A dial tone is a telephony signal used to indicate that the telephone exchange is working, has recognized an off-hook, and is ready to accept a call." );
+        _de_dialtone.setDescription( DIALTONE_DESC );
 
         _jp_dialtone = new ToneSequence( parent );
         _jp_dialtone.addSegment( 250, 400 );
-        _jp_dialtone.setDescription(
-                "A dial tone is a telephony signal used to indicate that the telephone exchange is working, has recognized an off-hook, and is ready to accept a call." );
+        _jp_dialtone.setDescription( DIALTONE_DESC );
 
         _jp_ringback = new ToneSequence( parent );
         _jp_ringback.addSegment( 1000, 384, 416 );
         _jp_ringback.addSegment( 2000, 0 );
-        _jp_ringback.setDescription(
-                "A ringback tone is an audible indication that is heard on the telephone line by the caller while the phone they are calling is being rung. It is normally a repeated tone, designed to assure the calling party that the called party's line is ringing, although the ring-back tone may be out of sync with the ringing signal." );
-
-        _it_dialtone = new ToneSequence( parent );
-        _it_dialtone.addSegment( 1000, 425);
-        _it_dialtone.addSegment( 4000, 0 );
-        _it_dialtone.setDescription(
-                "A dial tone is a telephony signal used to indicate that the telephone exchange is working, has recognized an off-hook, and is ready to accept a call." );
+        _jp_ringback.setDescription( RINGBACK_DESC );
 
         _it_ringback = new ToneSequence( parent );
-        _it_ringback.addSegment( 600, 425 );
-        _it_ringback.addSegment( 1000, 0 );
-        _it_ringback.addSegment( 200, 425 );
-        _it_ringback.addSegment( 200, 0 );
-        _it_ringback.setDescription(
-                "A ringback tone is an audible indication that is heard on the telephone line by the caller while the phone they are calling is being rung. It is normally a repeated tone, designed to assure the calling party that the called party's line is ringing, although the ring-back tone may be out of sync with the ringing signal." );
+        _it_ringback.addSegment( 1000, ITALY_FREQ );
+        _it_ringback.addSegment( 4000, 0 );
+        _it_ringback.setDescription( RINGBACK_DESC );
+
+        _it_dialtone = new ToneSequence( parent );
+        _it_dialtone.addSegment( 200, ITALY_FREQ );
+        _it_dialtone.addSegment( 200, 0 );
+        _it_dialtone.addSegment( 600, ITALY_FREQ );
+        _it_dialtone.addSegment( 1000, 0 );
+        _it_dialtone.setDescription( DIALTONE_DESC );
 
     }
 }
