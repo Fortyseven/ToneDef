2f09563fe38a0bda9cfa582153480e492b770697
Fortyseven <fortyseven@gmail.com>
2015-12-10 15:24:33 -0500

    Added more tones for USA

--- MainApp/src/main/java/com/bytestemplar/tonedef/international/CountryTonesRepository.java
+++ MainApp/src/main/java/com/bytestemplar/tonedef/international/CountryTonesRepository.java
@@ -109,6 +109,12 @@ public class CountryTonesRepository
         country.addSequence( _ownerActivity.getString( R.string.busy ), ts );
 
         ts = new ToneSequence( _ownerActivity );
+        ts.addSegment( 250, 480, 620 )
+          .addSegment( 250, 0 )
+          .setDescription( "" );
+        country.addSequence( _ownerActivity.getString( R.string.congestion_fast_busy_disconnect ), ts );
+
+        ts = new ToneSequence( _ownerActivity );
         ts.addSegment( 380, 914 )
           .addSegment( 276, 1429 )
           .addSegment( 380, 1777 )
@@ -148,6 +154,40 @@ public class CountryTonesRepository
           .addSegment( 6000, 350, 440 )
           .setDescription( "" );
         country.addSequence( _ownerActivity.getString( R.string.recall_tone ), ts );
+
+        ts = new ToneSequence( _ownerActivity );
+        ts.addSegment( 300, 440 )
+          .addSegment( 10000, 0 )
+          .setDescription( "" );
+        country.addSequence( _ownerActivity.getString( R.string.call_waiting ), ts );
+
+        ts = new ToneSequence( _ownerActivity );
+        ts.addSegment( 100, 350, 440 )
+          .addSegment( 100, 0 )
+          .addSegment( 100, 350, 440 )
+          .addSegment( 100, 0 )
+          .addSegment( 100, 350, 440 )
+          .addSegment( 2000, 0 )
+          .setDescription( "" );
+        country.addSequence( _ownerActivity.getString( R.string.positive_accepted ), ts );
+
+        ts = new ToneSequence( _ownerActivity );
+        ts.addSegment( 500, 200 )
+          .addSegment( 6000, 0 )
+          .setDescription( "" );
+        country.addSequence( _ownerActivity.getString( R.string.number_unobtainable ), ts );
+
+        ts = new ToneSequence( _ownerActivity );
+        ts.addSegment( 500, 440 )
+          .addSegment( 5000, 0 )
+          .setDescription( "" );
+        country.addSequence( _ownerActivity.getString( R.string.record ), ts );
+
+        ts = new ToneSequence( _ownerActivity );
+        ts.addSegment( 500, 1400 )
+          .addSegment( 14500, 0 )
+          .setDescription( "" );
+        country.addSequence( _ownerActivity.getString( R.string.warning ), ts );
 
         country.setFlagDrawable( R.drawable.flag_usa );
 
